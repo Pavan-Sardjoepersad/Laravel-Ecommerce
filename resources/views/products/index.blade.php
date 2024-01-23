bd1dcf5bf3da7ba994945e0ff58930f330e7ca83
Pavan <pavan.sardjoepersad@gmail.com>
2024-01-23 14:44:26 +0100

    Added dynamic pagination and images

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,16 +1,24 @@
 <x-layout>
 
-    <div class="flex flex-row w-full h-screen items-center justify-center">
+
+    
+    <div class="bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r flex flex-row w-full h-screen items-center justify-center">
         @foreach ($products as $product)
             {{-- <a href="/products/{{ $product->slug }}"> --}}
-            <a href="/products/{{$product->slug}}">    
-                <div class="px-20 pt-56">
+            <div
+                class="flex items-center flex-col mx-auto mt-11 w-80 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+                <img class="h-48 mb-16 w-full object-cover object-center" src="/images/{{$product->slug}}.png"
+                    alt="Product Image" />
+                <a href="/products/{{ $product->slug }}">
+
                     <button
-                        class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                        class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-2 mb-8">
+                        {{-- class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-2 mb-8"> --}}
                         {{ $product->name }}
                     </button>
-                </div>
-            </a>
+
+                </a>
+            </div>
         @endforeach
     </div>
 
@@ -18,3 +26,5 @@
 
 
 </x-layout>
+
+
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,12 +1,26 @@
 <x-layout>
 
-    <h1>{{ $product->name }} </h1>
-    <p>Description: {{ $product->description }}</p>
+    <div
+        class="mx-auto mt-11 w-80 transform overflow-hidden rounded-lg dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+        <img class="h-48 w-full object-cover object-center"
+            src="/images/{{$product->slug}}.png"
+            alt="Product Image" />
+        <div class="p-4">
+            <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900">{{$product->name}}</h2>
+            <p class="mb-2 text-base dark:text-gray-300 text-gray-700">{{$product->description}}</p>
+            <div class="flex items-center">
+                <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">{{$product->price}}</p>
+                <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">{{$product->price - ($product->price / 100) * 10}}</p>
+                <p class="ml-auto text-base font-medium text-green-500">10% off</p>
+            </div>
+        </div>
+    </div>
 
     <a href="/products">
-        <button 
+        <button
             class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
             Go back to Products Page</button>
     </a>
 
 </x-layout>
+
